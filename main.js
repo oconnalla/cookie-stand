c8f99ff451b00633dbad39be354a4c220a49cbc8
oconnalla <oconnalla@gmail.com>
2018-09-22 23:51:03 -0700

    made some changes to files and started adding objects

---
+++ main.js
@@ -0,0 +1,30 @@
+var store = [
+  {
+    location: '1st and Pike',
+    minCustomer: 23,
+    maxCustomer: 65,
+    avgSale: 6.3
+
+  }, {
+    location: 'SeaTac Airport',
+    minCustomer: 3,
+    maxCustomer: 24,
+    avgSale: 1.2
+
+  }, {
+    location: 'Seattle Center',
+    minCustomer: 11,
+    maxCustomer: 38,
+    avgSale: 3.7
+  }, {
+    location: 'Capitol Hill',
+    minCustomer: 20,
+    maxCustomer: 38,
+    avgSale: 2.3
+  }, {
+    location: 'Alki',
+    minCustomer: 2,
+    maxCustomer: 16,
+    avgSale: 4.6
+  }
+];
---
+++ sales.js
@@ -0,0 +1,3 @@
+function getRandomInt(maxCustomer) {
+return Math.floor(Math.random() * Math.floor(maxCustomer-minCustomer)+ minCustomer) +minCustomer;
+}
